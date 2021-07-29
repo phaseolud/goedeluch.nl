315f6087b2c800711ce9df51e051482bcdaffee1
phaseolud <loekvanleeuwen@outlook.com>
2021-07-29 11:55:25 +0200

    Add hover animation

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -23,14 +23,16 @@
     <div class="flex items-center justify-center py-4 bg-primary-400 flex-col md:flex-row">
         <p class="text-white text-lg font-semibold mb-2 md:mb-0">Zoek jouw broodje:</p>
         <form method="GET" action="#" class="flex items-center">
+            <div class="relative">
             <x-input class="ml-4"/>
-            <button class="bg-white text-primary-400 px-2 py-2" type="submit">
+            <button class=" absolute bg-white text-primary-400  right-2 inset-y-0 " type="submit">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                      stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
                 </svg>
             </button>
+            </div>
         </form>
     </div> {{-- Search --}}
 
